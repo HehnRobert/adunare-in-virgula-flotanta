ba47d60fb89cdf451d91fc4f8dcbd6f33388d7bb
HehnRobert <h_robert_f@yahoo.com>
2014-05-06 19:53:56 +0300

    Program terminat - problema semne diferite.

--- Adunare in virgula flotanta/Source.c
+++ Adunare in virgula flotanta/Source.c
@@ -18,6 +18,9 @@ void read()
 	fscanf(f, "%s %s", n1, n2);
 	fclose(f);
 	//printf("%s\n%s\n\n", n1, n2); -> TEST PENTRU CITIRE IN CHAR !ok!
+	f = fopen("in.txt", "w");
+	fprintf(f, "%s\n%s", n1, n2);
+	fclose(f);
 }
 
 void set()
@@ -66,7 +69,7 @@ int transexp(int v[8])//calculez exponentul sub forma de int (din baza 2 in baza
 	return (n - 127);
 }
 
-int swap(unsigned int *m1,unsigned int *m2, int *e1, int *e2,int *rez)//daca trebuie, fac swap si stabilesc exponentul rezultatului
+int swap(unsigned int *m1,unsigned int *m2, int *e1, int *e2,int *rez,int *ifs)//daca trebuie, fac swap si stabilesc exponentul rezultatului
 {//TESTAT - OK
 	if (((*e1) - (*e2)) < 0)
 	{
@@ -81,6 +84,7 @@ int swap(unsigned int *m1,unsigned int *m2, int *e1, int *e2,int *rez)//daca tre
 		aux1 = numar1.semn;
 		numar1.semn = numar2.semn;
 		numar2.semn = aux1;
+		(*ifs) = 1;
 	}
 	(*rez) = (*e1);
 	return (*e1) - (*e2);
@@ -140,10 +144,114 @@ unsigned int sum(unsigned int mantn1, unsigned int mantn2, unsigned int *rez)//c
 	return verifbit((*rez), 24);//returnez daca am carry out
 }
 
+void adjust_r_s(unsigned int *b, int d, unsigned int *r, unsigned int *s,unsigned int z0,unsigned int shift)
+{
+	if (d == 1)
+	{
+		(*r) = z0;
+		if ((*b)) (*s) = 1;
+		else (*s) = 0;
+	}
+	else if (d == 0)
+	{
+		(*r) = verifbit((*b), 0);
+		(*b) >> 1;
+		if ((*b)) (*s) = 1;
+		else (*s) = 0;
+	}
+	else if (d == -1)
+	{
+		(*r) = verifbit((*b), 0);
+		(*b) >> 1;
+		(*s) = 0;
+		for (int i = 0; i < shift; i++)
+		{
+			if (verifbit((*b), 0)) {
+				(*s) = 1; i = shift;
+			}
+			(*b) >> 1;
+		}
+	}
+	else
+	{
+		(*r) = 0;
+		(*s) = 0;
+	}
+}
+
+void rounding(unsigned int r,unsigned int s,unsigned int *zm,int *exp)
+{
+	unsigned int zon = verifbit((*zm), 0);
+	if (r&(zon | s))
+	{
+		(*zm)++;
+	}
+	if (verifbit((*zm), 24))
+	{
+		(*zm) >> 1;
+		(*exp)++;
+	}
+}
+
+void sign_result(int ifs,int ifc)
+{
+	if (ifs)
+	{
+		if ((numar1.semn = 0) && (numar2.semn = 1)) rezultat.semn = 1;
+		if ((numar1.semn = 1) && (numar2.semn = 0)) rezultat.semn = 0;
+	}
+	else
+	{
+		if (!ifc)
+		{
+			if ((numar1.semn = 0) && (numar2.semn = 1)) rezultat.semn = 0;
+			if ((numar1.semn = 1) && (numar2.semn = 0)) rezultat.semn = 1;
+		}
+		else
+		{
+			if ((numar1.semn = 0) && (numar2.semn = 1)) rezultat.semn = 1;
+			if ((numar1.semn = 1) && (numar2.semn = 0)) rezultat.semn = 0;
+		}
+	}
+}
+
+void pack(int expr,unsigned int mantr)
+{
+	int i,vecexp[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }, vecmant[23] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	f = fopen("in.txt", "a");
+	fprintf(f, "\n\nRezultat:\n");
+	expr += 127;
+	fprintf(f, "%d", rezultat.semn);
+	//Transformarea exponentului
+	i = 7;
+	while (expr)
+	{
+		vecexp[i--] = expr % 2;
+		expr /= 2;
+	}
+	//Scriere exponent in fisier
+	for (i = 0; i < 8; i++)
+		fprintf(f, "%d", vecexp[i]);
+	//Transformarea mantissei
+	i = 22;
+	mantr = mantr&(~(1 << 23));
+	while (mantr)
+	{
+		vecmant[i--] = mantr % 2;
+		mantr /= 2;
+	}
+	//Scriere mantissa in fisier
+	for (i = 0; i < 23; i++)
+	{
+		fprintf(f, "%d", vecmant[i]);
+	}
+	fclose(f);
+}
+
 int main()
 {
-	unsigned int mantn1, mantn2, mantrez, exprez, shift, cout, bit;//bit -> (g,r,s)
-	int expn1, expn2, test = 0;
+	unsigned int mantn1, mantn2, mantrez, shift, cout, bit, R, S,z0=0;//bit -> (g,r,s)
+	int expn1, expn2, exprez, test = 0, deplasare = 0, ifswap = 0, ifcomplement = 0;
 	read();
 	set();
 	mantn1 = transmant(numar1.mantissa);
@@ -151,7 +259,7 @@ int main()
 	expn1 = transexp(numar1.exponent);
 	expn2 = transexp(numar2.exponent);
 	//printf("\n\n%u\n%u\n%d\n%d\n\n", mantn1, mantn2, expn1, expn2);
-	shift = swap(&mantn1, &mantn2, &expn1, &expn2, &exprez);//pasul 2
+	shift = swap(&mantn1, &mantn2, &expn1, &expn2, &exprez,&ifswap);//pasul 2
 	//printf("\n\n%u\n%u\n%d\n%d\n%d\n\n", mantn1, mantn2, expn1, expn2,shift); ->TESTARE SWAP !ok!
 	if (numar1.semn == numar2.semn)//preshifting
 	{
@@ -168,40 +276,59 @@ int main()
 	if (numar1.semn != numar2.semn && cout == 0 && verifbit(mantrez, 23))//pasul 5
 	{
 		sign(&mantrez);
+		ifcomplement = 1;
 	}
-	/*if (cout && numar1.semn != numar2.semn)
-	{
-		if (verifbit(mantrez, 22) == 1)
-		{
-			mantrez = ((mantrez << 1) | verifbit(bit, 0));
-			exprez--;
-		}
-	}*/
 	if (cout && numar1.semn == numar2.semn)//pasul 6
 	{
-		bit = bit << 1 | verifbit(mantrez, 0);
-		mantrez = ((mantrez >> 1) | (1 << 23));//nu mai avem nevoie de g -> r devinte ce am scos din mantrez si restul este s
+		//bit = bit << 1 | verifbit(mantrez, 0);
+		z0 = verifbit(mantrez, 0);
+		mantrez = ((mantrez >> 1) | (1 << 23));//nu mai avem nevoie de g -> r devine ce am scos din mantrez si restul este s
 		exprez++;
+		deplasare = 1;
 	}
 	else if (!cout && numar1.semn == numar2.semn)
 	{
 		if (!verifbit(mantrez, 23)) //daca este denormalizat
 		{
+			deplasare = -1;
 			mantrez = ((mantrez << 1) | verifbit(bit, 0));//am introdus in mantrez g-ul si este eliminat din bit...g nu mai conteaza
 			exprez--;
 			bit = bit >> 1;
+			shift--;
 			while (verifbit(mantrez, 23) == 0)
 			{
 				mantrez = (mantrez << 1);
 				exprez--;
+				deplasare--;
 			}
 		}
-	}//!!!!!!!bit=***sr!!!!!!!!
-	//round to nearest!
+	}
+
+
+
+
+	//-----------------Pasul 7-----------------//
+	adjust_r_s(&bit, deplasare, &R, &S,z0,shift);
+	//=========================================//
+
+	//-----------------Pasul 8-----------------//
+	rounding(R,S,&mantrez,&exprez);
+	//=========================================//
+
+	//-----------------Pasul 9-----------------//
+	if (numar1.semn != numar2.semn) sign_result(ifswap,ifcomplement);
+	//=========================================//
+
+	//-----------------Pasul 10----------------//
+	pack(exprez, mantrez);
+	//=========================================//
+
+
+
 
 	//*********************TEST*********************//
 	printf("\n\n\n%d  %u  %u", rezultat.semn,exprez,mantrez);
 	printf("\n\n\t\t\t\t\t\tProgram incheiat cu succes!");
 	_getch();
 	return 0;
-}
+}//ATENTIE, am schimbat exprez din unsigned int int INT
